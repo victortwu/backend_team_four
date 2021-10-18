2b515ab772fb39ccc09759e5b258bb38fcd3b30c
victortwu <victor607@gmail.com>
2021-10-17 17:38:58 -0700

    cleaned it up a bit, will make bacode scan calls from the front end

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const axios = require('axios')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const CloudmersiveBarcodeapiClient = require('cloudmersive-barcodeapi-client')
-const defaultClient = CloudmersiveBarcodeapiClient.ApiClient.instance
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
 
 const PORT = process.env.PORT
 
@@ -51,46 +47,7 @@ db.on('connected', ()=> { console.log('CONNECTED TO MONGO')})
 db.on('disconnected', ()=> {console.log('disconnected...')})
 
 
-// testing cloud immersive ===================================
 
-
-//Configure API key authorization: Apikey
-
-const Apikey = defaultClient.authentications['Apikey'];
-Apikey.apiKey = process.env.CLOUDMERSIVE_APIKEY;
-// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-Apikey.apiKeyPrefix = 'Token';
-
-const apiInstance = new CloudmersiveBarcodeapiClient.BarcodeScanApi();
-
-//const imageFile = "/path/to/file.txt"; // File | Image file to perform the operation on.  Common file formats such as PNG, JPEG are supported.
-
-
-const callback = function(error, data, response) {
-  if (error) {
-    console.log('----- Heres an error:')
-    console.error(error);
-  } else {
-    console.log('API called successfully. Returned data: ' + data);
-  }
-};
-// apiInstance.barcodeScanImage(imageFile, callback);
-app.post('/scan', upload.single('imageFile'), (req, res, next) =>  {
-
-  const imageFile = req.file
-  console.log('Came through........', imageFile)
-
-  apiInstance.barcodeScanImage(imageFile, callback);
-  console.log('Is this the data trying to come back?????? ', callback)
-  // res.send(callback)
-})
-
-
-// testing recycle place api=====================================================
-
-
-
-//===========================================================
 
 // routes
 
