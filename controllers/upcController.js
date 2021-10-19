9560c5727717d3ffadbb7debbb9f77ebcfb8bc8c
Tony Shifflett <charles.l.shifflett@gmail.com>
2021-10-19 00:47:43 -0500

    modified api call in upcController.js to use Earth-9111

--- controllers/upcController.js
+++ controllers/upcController.js
@@ -3,15 +3,17 @@ const upc = express.Router()
 const axios = require('axios')
 
 const upcBaseURL = process.env.UPC_BASEURL
+const upcEarth911URL = process.env.UPC_EARTHURL
 const upcApiKey = '/?apikey=' + process.env.UPC_APIKEY
-
+const upcEarthApiKey = '&api_key=' + process.env.RECYCLE_APIKEY
 
 upc.get('/:code', async(req, res) => {
       try{
 
         const query = req.params.code
 
-        axios.get(upcBaseURL + query + upcApiKey)
+        // axios.get(upcBaseURL + query + upcApiKey)
+        axios.get(upcEarth911URL + query + upcEarthApiKey)
         .then(res => {
           console.log(res)
           return res.data
