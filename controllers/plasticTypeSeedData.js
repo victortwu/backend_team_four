013f232b164d1a756082a747a62b1e6868131dcd
victortwu <victor607@gmail.com>
2021-10-24 14:53:59 -0700

    added info to seed route, dropped and re seeded locally, reconfigured schema accordingly

--- controllers/plasticTypeSeedData.js
+++ controllers/plasticTypeSeedData.js
@@ -2,56 +2,91 @@ const express = require('express')
 const seed = express.Router()
 const PlasticType = require('../models/plasticType.js')
 
-
+// credit: acme plastics.com
 seed.get('/', (req, res) => {
   PlasticType.create([
     {
-      recycleNumber: '1',   //-------- ONE
+      recycleNumber: '1',  //-------- ONE
+      recycleCode: 'PETE',
+      plasticName: 'polyethylene terephthalate',
       generalDescription: 'General description 1',
-      recycleInfoOptionOne: 'Option One info field 1',
-      recycleInfoOptionTwo: 'Option Two info field 1',
+      canRecycle: 'Yes',
+      recycleInfoOne: 'Most areas recycle #1',
+      recycleInfoTwo: 'Contact your local recycling company for more info',
+      productExamples: 'soft drink bottles, mineral water containers, and fruit juice containers, and cooking oil containers',
+      recycledExamples: 'containers, carpet, furniture',
       recycleImages: []
     },
     {
       recycleNumber: '2',    //-------- TWO
+      recycleCode: 'HDPE',
+      plasticName: 'high-density polyethylene',
       generalDescription: 'General description 2',
-      recycleInfoOptionOne: 'Option One info field 2',
-      recycleInfoOptionTwo: 'Option Two info field 2',
+      canRecycle: 'Yes',
+      recycleInfoOne: 'Most areas recycle #2',
+      recycleInfoTwo: 'Contact your local recycling company for more info',
+      productExamples: 'cleaning agents, milk, detergents, washing soap',
+      recycledExamples: 'pipes, oil bottles, pens, detergent bottles',
       recycleImages: []
     },
     {
       recycleNumber: '3',    //-------- THREE
+      recycleCode: 'V',
+      plasticName: 'polyvinyl chloride',
       generalDescription: 'General description 3',
-      recycleInfoOptionOne: 'Option One info field 3',
-      recycleInfoOptionTwo: 'Option Two info field 3',
+      canRecycle: 'No',
+      recycleInfoOne: 'This type of plastic is rarely recycled',
+      recycleInfoTwo: 'Contact your local recycling company for more info',
+      productExamples: 'bubble foils, trays for sweets and fruit',
+      recycledExamples: 'speed bumps, roadway gutters, cables',
       recycleImages: []
     },
     {
       recycleNumber: '4',    //-------- FOUR
+      recycleCode: 'LDPE',
+      plasticName: 'low-density polyethylene',
       generalDescription: 'General description 4',
-      recycleInfoOptionOne: 'Option One info field 4',
-      recycleInfoOptionTwo: 'Option Two info field 4',
+      canRecycle: 'No',
+      recycleInfoOne: 'This type of plastic is not usually recycled',
+      recycleInfoTwo: 'Contact your local recycling company for more info',
+      productExamples: 'shopping bags, highly-resistant sacks, crushed bottles',
+      recycledExamples: 'floor tiles, shipping envelopes',
       recycleImages: []
     },
     {
       recycleNumber: '5',    //-------- FIVE
+      recycleCode: 'PP',
+      plasticName: 'polypropylene plastic',
       generalDescription: 'General description 5',
-      recycleInfoOptionOne: 'Option One info field 5',
-      recycleInfoOptionTwo: 'Option Two info field 5',
+      canRecycle: 'Maybe',
+      recycleInfoOne: 'Increasingly being accepted in curbside recycling programs',
+      recycleInfoTwo: 'Contact your local recycling company for more info',
+      productExamples: 'furniture, luggage, toys',
+      recycledExamples: 'ketchup bottles, medicine bottles',
       recycleImages: []
     },
     {
       recycleNumber: '6',    //-------- SIX
+      recycleCode: 'PS',
+      plasticName: 'polystyrene',
       generalDescription: 'General description 6',
-      recycleInfoOptionOne: 'Option One info field 6',
-      recycleInfoOptionTwo: 'Option Two info field 6',
+      canRecycle: 'Probably not',
+      recycleInfoOne: 'This type of plastic is not usually recycled',
+      recycleInfoTwo: 'Contact your local recycling company for more info',
+      productExamples: 'refrigerator trays, cosmetic bags, costume jewelry, CD cases',
+      recycledExamples: 'make egg cartons, take-out containers, rulers',
       recycleImages: []
     },
     {
       recycleNumber: '7',    //-------- SEVEN
+      recycleCode: 'OTHER',
+      plasticName: 'acrylic, polycarbonate, polylactic fibers',
       generalDescription: 'General description 7',
-      recycleInfoOptionOne: 'Option One info field 7',
-      recycleInfoOptionTwo: 'Option Two info field 7',
+      canRecycle: 'Maybe',
+      recycleInfoOne: 'Requires more research on your part',
+      recycleInfoTwo: 'Contact your local recycling company for more info',
+      productExamples: 'fiberglass, nylon, acrylic',
+      recycledExamples: '',
       recycleImages: []
     }
   ], (err, data) => {
--- models/plasticType.js
+++ models/plasticType.js
@@ -3,9 +3,14 @@ const { Schema, model } = mongoose
 
 const plasticTypeSchema = new Schema({
   recycleNumber: String,
+  recycleCode: String,
+  plasticName: String,
   generalDescription: String,
-  recycleInfoOptionOne: String,
-  recycleInfoOptionTwo: String,
+  canRecycle: String,
+  recycleInfoOne: String,
+  recycleInfoTwo: String,
+  productExamples: String, // <--- should be an array in the future
+  recycledExamples: String, // <--- should be an array in the future
   recycleImages: [{type: String}]
 })
 
