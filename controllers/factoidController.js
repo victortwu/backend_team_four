2a775853903e13b358a49704d10ac3ca11c07ba6
victortwu <victor607@gmail.com>
2021-10-28 08:31:18 -0700

    refactored factoid routes, now has full crud as well

--- controllers/factoidController.js
+++ controllers/factoidController.js
@@ -7,17 +7,24 @@ const Factoids = require('../models/factoids.js')
 
 factoid.get('/seed', (req, res) => {
   Factoids.create([
-    { factoids: [
-      'Recycling 10 plastic bottles saves enough energy to power a laptop for more than 25 hours, according to the EPA.',
-      'Only 8% of discarded plastics were recycled in 2017, according to the EPA.',
-      'According to the National Waste & Recycling Association, 25% of what ends up in recycling bins is contaminated and must be thrown out.',
-      'Plastic accounts for roughly 12% of the total municipal solid waste in the US.',
-      'Plastic takes hundreds of years to decompose',
-      'Only 9% of plastic is recycled, according to National Geographic.',
-      'According to a United Nations report, more than 220 million tons of plastic are produced each year.',
-      'According to Statista, 92% of 18-34 year olds recycle in the US.',
-      'Americans throw out 2.5 million plastic bottles every hour, according to Recycle Across America.',
-    ] }
+    { factoid: 'Recycling 10 plastic bottles saves enough energy to power a laptop for more than 25 hours, according to the EPA.' },
+
+    { factoid: 'Only 8% of discarded plastics were recycled in 2017, according to the EPA.' },
+
+    { factoid: 'According to the National Waste & Recycling Association, 25% of what ends up in recycling bins is contaminated and must be thrown out.' },
+
+    { factoid: 'Plastic accounts for roughly 12% of the total municipal solid waste in the US.' },
+
+    { factoid: 'Plastic takes hundreds of years to decompose' },
+
+    { factoid: 'Only 9% of plastic is recycled, according to National Geographic.' },
+
+    { factoid: 'According to a United Nations report, more than 220 million tons of plastic are produced each year.' },
+
+    { factoid: 'According to Statista, 92% of 18-34 year olds recycle in the US.' },
+
+    { factoid: 'Americans throw out 2.5 million plastic bottles every hour, according to Recycle Across America.' },
+
   ],(err, data) => {
     if(err) {
       console.error(err.message)
@@ -28,7 +35,6 @@ factoid.get('/seed', (req, res) => {
 
 
 factoid.get('/', (req, res)=> {
-  console.log('hit get route')
   Factoids.find({}, (err, foundFactoids) => {
     if(err){
       res.status(400).json(err)
@@ -37,5 +43,38 @@ factoid.get('/', (req, res)=> {
     }
   })
 })
+
+
+factoid.post('/', (req, res)=> {
+  Factoids.create(req.body, (err,createdFactoid) => {
+    if (err) {
+      res.status(400).json(err)
+    } else {
+      res.status(200).json(createdFactoid)
+    }
+  })
+})
+
+factoid.put('/:id', (req, res)=> {
+  Factoids.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedFactoid)=> {
+    if(err) {
+      res.status(400).json(err)
+    } else {
+      res.status(200).json('Factoid updated successfully')
+    }
+  })
+})
+
+factoid.delete('/:id', (req, res) => {
+  Factoids.findByIdAndDelete(req.params.id, (err, deletedData)=> {
+    if(err) {
+      res.status(400).json(err)
+    } else {
+      res.status(200).json(`${deletedData} has been successfully deleted.`)
+    }
+  })
+})
+
+
 
 module.exports = factoid
--- models/factoids.js
+++ models/factoids.js
@@ -3,7 +3,7 @@ const { Schema, model } = mongoose
 
 
 const factoidSchema = new Schema({
-  factoids: [],
+  factoid: String,
 })
 
 
